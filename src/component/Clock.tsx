945bd8dafe177571c2fce1e2d2675089bd72ab1e
Shota Ninomiya <a2038toh7s1124@gmail.com>
2020-02-27 20:34:48 +0900

    Clockの中で時間の計測をするように変更

--- src/component/Clock.tsx
+++ src/component/Clock.tsx
@@ -1,11 +1,39 @@
 import React from "react"
 
-export class Clock extends React.Component {
+interface Props {
+}
+interface State {
+    date: Date;
+}
+
+export class Clock extends React.Component<Props, State> {
+    timeId: any;
+    
+    constructor(props: Props) {
+        super(props);
+        this.state = { date: new Date() };
+    }
+
+    componentDidMount() { // 出力が DOM にレンダーされた後に実行されます
+        this.timeId = setInterval(
+          () => this.tick(),
+          1000
+        ); 
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timeId);
+    }
+
+    tick(){
+        this.setState({date: new Date()});
+    }
+
     render() {
         return (
             <div>
-                <span>現在時刻:</span><span>{new Date().toLocaleTimeString()}</span>
+                <span>現在時刻:</span><span>{this.state.date.toLocaleTimeString()}</span>
             </div>
         );
-    } 
+    }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,7 @@ import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-// ref: https://ja.reactjs.org/docs/rendering-elements.html#updating-the-rendered-element
-setInterval(
-   ()=> ReactDOM.render(<App />, document.getElementById('root'))
-   , 1000);
+ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
